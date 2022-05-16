2497af7953313e9ab6a95bf7e262899d9f5afb03
dangchien <82793130+vdchien98@users.noreply.github.com>
2022-05-17 01:11:42 +0700

    eleventhcommit tichhopReact-Slider-Carousel vs srcollbar vs Customize Tùy Chỉnh React-Slider-Carousel

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,6 +18,7 @@ import System from '../routes/System';
 import { CustomToastCloseButton } from '../components/CustomToast';
 import ConfirmModal from '../components/ConfirmModal';
 import HomePage from './HomePage/HomePage.js';
+import CustomScrollbars from '../../src/components/CustomScrollbars';
 class App extends Component {
     handlePersistorState = () => {
         const { persistor } = this.props;
@@ -45,14 +46,16 @@ class App extends Component {
                         <ConfirmModal />
                         {this.props.isLoggedIn && <Header />}
 
-                        <span className="content-container">
-                            <Switch>
-                                <Route path={path.HOME} exact component={Home} />
-                                <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
-                                <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
-                                <Route path={path.HOMEPAGE} component={HomePage} />
-                            </Switch>
-                        </span>
+                        <div className="content-container">
+                            <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
+                                <Switch>
+                                    <Route path={path.HOME} exact component={Home} />
+                                    <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
+                                    <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                    <Route path={path.HOMEPAGE} component={HomePage} />
+                                </Switch>
+                            </CustomScrollbars>
+                        </div>
 
                         <ToastContainer
                             className="toast-container"
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import HomeHader from './HomeHader';
+import Specialty from './Section/Specialty';
 class HomePage extends Component {
     render() {
         return (
             <div>
                 <HomeHader />
+                <Specialty />
+                <div style={{ height: '300px' }}></div>
             </div>
         );
     }
